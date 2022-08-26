1fcadd1d3d8c0140ea9d109b6c586b0ca358dbcc
Akshaya <akshaya@Akshayas-MacBook-Air.local>
2022-08-26 16:07:51 +0530

    Changes

--- ingredients/api.js
+++ ingredients/api.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const express = require("express");
 const router = express.Router();
+const pg = require('pg');
 
 // client side static assets
 router.get("/", (_, res) => res.sendFile(path.join(__dirname, "./index.html")));
@@ -13,14 +14,23 @@ router.get("/client.js", (_, res) =>
  */
 
 // connect to postgres
+const pool = new pg.Pool({
+    user: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    password: 'lol',
+    database: 'recipeguru'
+})
 
 router.get("/type", async (req, res) => {
   const { type } = req.query;
   console.log("get ingredients", type);
 
   // return all ingredients of a type
+  const { rows } = await pool.query('SELECT * FROM ingredients WHERE type=$1;', [type])
+  res.json({rows}).end()
+  // res.status(501).json({ status: "not implemented", rows: [] });
 
-  res.status(501).json({ status: "not implemented", rows: [] });
 });
 
 router.get("/search", async (req, res) => {
@@ -30,8 +40,11 @@ router.get("/search", async (req, res) => {
 
   // return all columns as well as the count of all rows as total_count
   // make sure to account for pagination and only return 5 rows at a time
-
-  res.status(501).json({ status: "not implemented", rows: [] });
+  const { rows } = await pool.query(
+    'SELECT *, COUNT(*) OVER ()::INTEGER AS total_count FROM ingredients WHERE CONCAT(type,title) ILIKE $2 LIMIT 5 OFFSET $1', [page*5 ,`%${term}%`]
+  )
+  res.json({rows}).end()
+  // res.status(501).json({ status: "not implemented", rows: [] });
 });
 
 /**
--- recipes/api.js
+++ recipes/api.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const express = require("express");
 const router = express.Router();
+const pg = require("pg");
 
 // client side static assets
 router.get("/", (_, res) => res.sendFile(path.join(__dirname, "./index.html")));
@@ -22,15 +23,32 @@ router.get("/detail", (_, res) =>
  */
 
 // connect to postgres
+const pool = new pg.Pool({
+  user: 'postgres',
+  database: 'recipeguru',
+  host: 'localhost',
+  port: 5432,
+  password: 'lol'
+})
 
 router.get("/search", async function (req, res) {
   console.log("search recipes");
+
+  const { rows } = await pool.query(`SELECT DISTINCT ON (recipe_id)
+  r.recipe_id AS recipe_id, 
+  r.title AS title, 
+  COALESCE(rp.url, 'default.jpg') AS url
+  FROM recipes r 
+  LEFT JOIN recipes_photos rp
+  ON r.recipe_id = rp.recipe_id`)
+
+  res.json({ rows }).end();
 
   // return recipe_id, title, and the first photo as url
   //
   // for recipes without photos, return url as default.jpg
 
-  res.status(501).json({ status: "not implemented", rows: [] });
+  // res.status(501).json({ status: "not implemented", rows: [] });
 });
 
 router.get("/get", async (req, res) => {
@@ -41,17 +59,60 @@ router.get("/get", async (req, res) => {
   //    name the ingredient image `ingredient_image`
   //    name the ingredient type `ingredient_type`
   //    name the ingredient title `ingredient_title`
-  //
-  //
+   const ingredientsPromise = pool.query (`
+   SELECT 
+      i.image AS ingredient_image,
+      i.title AS ingredient_title,
+      i.type AS ingredient_type
+   FROM
+      recipe_ingredients ri
+    
+   INNER JOIN 
+      ingredients i
+   ON 
+      i.id = ri.ingredient_id
+   WHERE 
+      ri.recipe_id=$1;
+   `, 
+      [recipeId]
+    )
   // return all photo rows as photos
   //    return the title, body, and url (named the same)
-  //
-  //
+   const photoPromise = pool.query (`
+   SELECT 
+      r.title AS title,
+      r.body AS body,
+      COALESCE(rp.url, 'default.jpg') AS url
+   FROM 
+      recipes_photos rp
+   INNER JOIN
+      recipes r
+    ON
+      r.recipe_id = rp.recipe_id
+    WHERE 
+      rp.recipe_id=$1;
+    `, 
+      [recipeId]
+    )
   // return the title as title
   // return the body as body
   // if no row[0] has no photo, return it as default.jpg
+  const [ingredientResponse, photoResponse] = await Promise.all([
+    ingredientsPromise,
+    photoPromise
+  ])
 
-  res.status(501).json({ status: "not implemented" });
+  const ingredientRow = ingredientResponse.rows;
+  const photoRows = photoResponse.rows;
+  
+  res.json({ 
+    ingredients: ingredientRow,
+    title: photoRows[0].title,
+    body: photoRows[0].body,
+    photos: photoRows.map((photo) => photo.url),
+   }).end();
+
+  // res.status(501).json({ status: "not implemented" });
 });
 /**
  * Student code ends here
